0784aedff802e629213b5b4bc909ee586b78f820
Ross Domke <ross@domke.io>
2019-12-19 16:12:00 -0500

    Initial Commit

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <trun/>
+  </div>
+</template>
+
+<script>
+import Trun from './components/Trun.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Trun,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/Trun.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Trun</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Trun',
+  components: {
+
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
