2d750a0a51f075105cfff4afda8f91079629cd6c
Ross Domke <ross@domke.io>
2020-01-04 18:03:06 -0500

    Restructuring app

--- client/main.js
+++ client/main.js
@@ -3,12 +3,14 @@ import Vue from 'vue';
 import VueSocketIO from 'vue-socket.io';
 import App from './App.vue';
 
+const connection = '//localhost:3000/'; // https://trun.domke.io
+
 // const SocketInstance = new Socketio('http://localhost:3000');
 
 // Vue.use(VueSocketIO, SocketInstance);
 Vue.use(new VueSocketIO({
   debug: true,
-  connection: 'https://trun-cycles.herokuapp.com/',
+  connection,
 }));
 
 Vue.config.productionTip = false;
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicPath: '/',
+  pages: {
+    entry: 'client/main.js',
+    template: 'public/index.html',
+    filename: 'index.html',
+    title: 'Trun'
+  }
+};
