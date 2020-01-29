f016473ca819aebd561d452465cb4fcbfbef14e5
Ross Domke <ross@domke.io>
2020-01-28 19:54:43 -0500

    Added styling and message container

--- client/components/Game.vue
+++ client/components/Game.vue
@@ -1,6 +1,12 @@
 <template>
-  <div ref="gameContainer">
-    <canvas ref="game" style="border: 1px solid black;"></canvas>
+  <div ref="gameContainer" class="game-container">
+    <div class="game-cover" v-if="gameStatus" >
+      <div class="game-message">
+        <h1>{{ game.name }}</h1>
+        <span>{{ gameStatus }}</span>
+      </div>
+    </div>
+    <canvas ref="game" class="game"></canvas>
   </div>
 </template>
 
@@ -13,6 +19,7 @@ export default {
     return {
       context: null,
       width: 400,
+      gameStatus: 'Waiting for game to start...',
     };
   },
   created() {
@@ -47,3 +54,41 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+  .game-container {
+    position: relative;
+  }
+  .game-cover {
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: rgba(0,0,0,.8);
+    z-index: 10;
+  }
+  .game-message {
+    background-color: white;
+    width: 100%;
+    min-height: 30px;
+    color: black;
+    text-align: center;
+    padding: 15px;
+    > h1 {
+      padding: 0;
+      margin: 2px;
+    }
+    > span {
+      padding: 0px;
+      margin: 0 0 5px;
+    }
+  }
+  .game-position {
+    position: relative;
+    z-index: 5;
+  }
+</style>
